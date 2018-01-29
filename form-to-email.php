e8eb2092613a4d6f75436355def77d946812b582
jdalton467 <jamesdalton463@gmail.com>
2018-01-29 16:50:28 -0500

    push

--- form-to-email.php
+++ form-to-email.php
@@ -21,8 +21,8 @@ $email_from = "jamesdalton463@gmail.com";
 $email_subject = "New Form submission";
 $email_body = "You have recieved a new message from the user $name. \n".
 	"email address: $visitor_email\n".
-	"Here is the message: \n $message".
-$to = "jamesdalton463@gmail.com"
+	"Here is the message: \n $message".;
+$to = "jamesdalton463@gmail.com";
 $headers = "From: $email_from \r\n";
 
 mail($to, $email_subject, $email_body, $headers); //Send email
