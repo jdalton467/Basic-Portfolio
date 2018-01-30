370bd6f7b045b70dd6eb7ac27bd7fd9e7515a056
jdalton467 <jamesdalton463@gmail.com>
2018-01-29 19:16:25 -0500

    push

--- form-to-email.php
+++ form-to-email.php
@@ -5,7 +5,7 @@
 if(!isset($_POST['submit']))
 {
 	//This page should not be accessed directly. Need to submit the form
-	echo "error; you need to submit the form!";
+	// echo "error; you need to submit the form!";
 }
 
 $name = $_POST['name'];
