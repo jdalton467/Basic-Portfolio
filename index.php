e493cc18712af05685050bcc109048892cc49cfb
jdalton467 <jamesdalton463@gmail.com>
2018-01-30 12:28:53 -0500

    push

--- index.php
+++ index.php
@@ -1,7 +1,19 @@
 
 <?php
 if($_POST["message"]){
-	mail("jamesdalton463@gmail.com", "Form to email message", $_POST["message"], "From: an@email.address");
+	$recipient="jamesdalton463@gmail.com";
+	$subject="Form to email message";
+	$sender=$_POST["sender"];
+	$senderEmail=$_POST["senderEmail"];
+	$message=$_POST["message"];
+
+	$mailBody="Name: $sender\nEmail:
+	$senderEmail\n\n$message";
+
+	mail($recipient, $subject, $mailBody, "From: $sender <$senderEmail>");
+
+	$thankYou ="<p>Thank you! Your message has been sent. </p>";
+
 } 
 ?>
 
